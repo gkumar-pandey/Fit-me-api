cb34d1ac36a8936b4aee7713c7e0b80a61934430
gkumar-pandey <gautamshekhar078@gmail.com>
2024-01-27 00:26:53 +0530

    connect app & initialize

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import { connectDb } from "./config/db";
+
 dotenv.config();
 const app: Express = express();
 app.use(express.json());
+connectDb();
 
-const PORT = process.env.PORT || 3000;
+const PORT: Number | String = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT:${PORT}`);
